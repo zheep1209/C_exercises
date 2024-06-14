c52f79586c0742154ef2c1ede4896502e36e77f8
西行寺岩羊 <663574252@qq.com>
2024-06-14 22:49:58 +0800

    2024年6月14日22:49:56

---
+++ sandbox.c
@@ -0,0 +1,16 @@
+//
+// Created by zheep on 2024/6/14.
+//
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main (){
+//        for (int i = 0; i < strlen(s);i++) {
+//            int num = 0;
+//            switch (s[i]) {
+//                case 'I':
+//                    num = 1;
+//                    break;
+//            }
+//        }
+//}
